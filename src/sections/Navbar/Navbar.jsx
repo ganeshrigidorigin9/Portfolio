fd9269b47f24685da6deebe20bf28c59ccc6ccfa
ganeshrigidorigin9 <ganeshganssh8@gmail.com>
2024-07-25 22:11:28 +0530

    first commit

--- src/sections/Navbar/Navbar.jsx
+++ src/sections/Navbar/Navbar.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaBars, FaTimes } from 'react-icons/fa';
 import styles from './NavbarStyles.module.css';
 
 function Navbar() {
   return (
     <nav className={styles.Navbar}>
+      <input type="checkbox" id="menu-toggle" className={styles.menuToggle} />
+      <label htmlFor="menu-toggle" className={styles.hamburger}>
+        <FaBars className={styles.bars} />
+        <FaTimes className={styles.times} />
+      </label>
       <ul className={styles.list}>
         <li className={styles.navbarItem}><Link className={styles.link} to="/#hero">About</Link></li>
         <li className={styles.navbarItem}><Link className={styles.link} to="/#projects">Projects</Link></li>
